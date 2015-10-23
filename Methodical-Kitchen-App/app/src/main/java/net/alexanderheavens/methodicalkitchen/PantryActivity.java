11e1be62a67369cc941b24cf99b8dc059df35cbd
Alexander Heavens <alexander.heavens@gmail.com>
2015-10-23 04:40:17 +0100

    Tweaks to the UI to get it running.

--- Methodical-Kitchen-App/app/src/main/java/net/alexanderheavens/methodicalkitchen/PantryActivity.java
+++ Methodical-Kitchen-App/app/src/main/java/net/alexanderheavens/methodicalkitchen/PantryActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class PantryActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantry);
+        System.out.println("PantryActivity Created");
     }
 
     @Override
